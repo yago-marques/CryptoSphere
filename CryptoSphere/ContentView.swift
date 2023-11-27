883056c4103fbb1a9da8a05e396bb24e84a61275
Yago Marques <84297648+yago-marques@users.noreply.github.com>
2023-11-27 14:15:42 -0300

    Initial Commit

---
+++ CryptoSphere/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CryptoSphere
+//
+//  Created by Yago Marques on 27/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CryptoSphere/CryptoSphereApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoSphereApp.swift
+//  CryptoSphere
+//
+//  Created by Yago Marques on 27/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoSphereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
