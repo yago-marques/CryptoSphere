2c7b49cf5cabf4a1feb2d7beb2b2d64bc578767a
Yago Marques <84297648+yago-marques@users.noreply.github.com>
2023-11-28 18:38:28 -0300

    🦄 feat | creating fallbacks

--- CryptoSphere/Source/Scenes/ListCoins/ListCoinsFeature.swift
+++ CryptoSphere/Source/Scenes/ListCoins/ListCoinsFeature.swift
@@ -16,31 +16,40 @@ struct ListCoinsFeature {
     struct State: Equatable {
         var coins = [DisplayedCoin]()
         var loading = false
+        var cacheUsed = false
+        var internetErrorAndCacheNotAvailable = false
     }
 
     enum Action {
-        case addNewCoinButtonTapped
         case onAppear
         case onAppearResponse(coins: [DisplayedCoin])
+        case apiFailureAndCacheAvailable(cachedCoins: [DisplayedCoin])
+        case apiAndCacheFailure
     }
 
     var body: some Reducer<State, Action> {
         Reduce { state, action in
             switch action {
-            case .addNewCoinButtonTapped:
-                addNewCoin(&state)
-                return .none
-
             case .onAppear:
                 state.loading = true
                 return .run { send in
-                    let coins = try await fetchCoins()
-                    await send(.onAppearResponse(coins: coins))
+                    try await fetchCoins(send)
                 }
 
             case .onAppearResponse(let coins):
                 state.loading = false
                 state.coins = coins
+                return .none
+
+            case .apiFailureAndCacheAvailable(cachedCoins: let cachedCoins):
+                state.loading = false
+                state.cacheUsed = true
+                state.coins = cachedCoins
+                return .none
+
+            case .apiAndCacheFailure:
+                state.loading = false
+                state.internetErrorAndCacheNotAvailable = true
                 return .none
             }
 
@@ -49,22 +58,22 @@ struct ListCoinsFeature {
 }
 
 private extension ListCoinsFeature {
-    func addNewCoin(_ state: inout ListCoinsFeature.State) {
-//        state.coins.append(.init(id: "julia", name: "julia", symbol: "oi"))
-    }
-
-    func fetchCoins() async throws -> [DisplayedCoin] {
+    func fetchCoins(_ send: Send<ListCoinsFeature.Action>) async throws {
         do {
             let businessCoins = try await coinLoader.fetchCoinList()
             let exchange = try await coinLoader.getDollarExchangeRate()
             let coins = businessCoins.map { CoinMapper.toDisplayed(from: $0, exchange: exchange) }
             try cacheManager.saveInCacheIfNeeded(coins)
 
-            return coins
+            await send(.onAppearResponse(coins: coins))
         } catch is APICallError {
             let cachedCoins = try cacheManager.tryFetchFromCache()
 
-            return cachedCoins
+            await send(.apiFailureAndCacheAvailable(cachedCoins: cachedCoins))
+        } catch {
+            await send(.apiAndCacheFailure)
         }
     }
+
+
 }
